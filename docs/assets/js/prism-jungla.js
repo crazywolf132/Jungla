01cebfe8df3823a2d3b2056a561873091031e77b
crazywolf132 <crazywolf132@gmail.com>
2020-06-01 15:55:02 +1000

    Added code languages

---
+++ docs/assets/js/prism-jungla.js
@@ -0,0 +1,21 @@
+Prism.languages.Jungla = {
+	string: {
+		pattern: /"(?:\\.|[^\\"\r\n])*"/,
+		greedy: true,
+	},
+	number: /(?:\B-|\b)\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
+	boolean: /\b(?:true|false|null)\b/,
+	// variable: /\$[a-z_]\w*/i,
+	variable: /@[a-z_]\w*/i,
+	keyword: /\b(?:STRING|NUMBER|LIST|OBJ|BOOL|#)\b/,
+	'attr-name': {
+		pattern: /[a-z_]\w*(?=\s*(?:\((?:[^()"]|"(?:\\.|[^\\"\r\n])*")*\))?:)/i,
+		greedy: true,
+	},
+	operator: {
+		pattern: /(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|and|@|#|\|[|=]?|or|is|\*=?|\/=?|%=?|\^=?|[?:~])/m,
+		lookbehind: true,
+	},
+	punctuation: /[!(){}\[\]=~,<>]/,
+	constant: /\b(?!ID\b)[A-Z][A-Z_\d]*\b/,
+};
