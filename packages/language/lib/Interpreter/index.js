cf8b7da85915206b8481c3b36af4f3a6a33baee5
bmoon-autoit <bmoon@auto-it.com>
2021-05-13 17:39:26 +1000

    fix: fixed crashing problem

--- packages/language/lib/Interpreter/index.js
+++ packages/language/lib/Interpreter/index.js
@@ -13,32 +13,34 @@ var _utils = require("../utils");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// To remove all cases of empty obj's
+const dive = obj => {
+  const newObj = {};
+  Object.keys(obj).forEach(key => {
+    const value = obj[key];
+
+    if (typeof value === 'object' && Object.keys(value).length >= 1) {
+      var _dive;
+
+      const deepDiveResult = (_dive = dive(value)) !== null && _dive !== void 0 ? _dive : {};
+
+      if (Object.keys(deepDiveResult).length >= 1) {
+        // This means that there are valid children here...
+        newObj[key] = deepDiveResult;
+      }
+    } else if (value !== null && value !== undefined) {
+      // We are going to assume that this is some form of value... so we will
+      // add it to the list
+      newObj[key] = value;
+    }
+  });
+};
+
 class Interpreter {
   constructor(ast, data) {
     this.ast = ast;
     this.data = data;
-    this.result = !Array.isArray(this.data) ? (0, _core.default)(this.ast, this.data) : this.data.map(d => (0, _core.default)(this.ast, d)).filter(i => Object.keys(i).length >= 1).map(this.dive).filter(i => i);
-  } // To remove all cases of empty obj's
-
-
-  dive(obj) {
-    const newObj = {};
-    Object.keys(obj).forEach(key => {
-      const value = obj[key];
-
-      if (typeof value === 'object' && Object.keys(value).length >= 1) {
-        const deepDiveResult = dive(value);
-
-        if (Object.keys(deepDiveResult).length >= 1) {
-          // This means that there are valid children here...
-          newObj[key] = deepDiveResult;
-        }
-      } else if (value !== null && value !== undefined) {
-        // We are going to assume that this is some form of value... so we will
-        // add it to the list
-        newObj[key] = value;
-      }
-    });
+    this.result = !Array.isArray(this.data) ? (0, _core.default)(this.ast, this.data) : this.data.map(d => (0, _core.default)(this.ast, d)).filter(i => Object.keys(i).length >= 1).map(dive).filter(i => i);
   }
 
 }
--- packages/language/src/Interpreter/index.js
+++ packages/language/src/Interpreter/index.js
@@ -2,6 +2,27 @@ import _ from 'lodash';
 import Core from './core';
 import { log } from '../utils';
 
+// To remove all cases of empty obj's
+const dive = (obj) => {
+	const newObj = {};
+
+	Object.keys(obj).forEach((key) => {
+		const value = obj[key];
+
+		if (typeof value === 'object' && Object.keys(value).length >= 1) {
+			const deepDiveResult = dive(value) ?? {};
+			if (Object.keys(deepDiveResult).length >= 1) {
+				// This means that there are valid children here...
+				newObj[key] = deepDiveResult;
+			}
+		} else if (value !== null && value !== undefined) {
+			// We are going to assume that this is some form of value... so we will
+			// add it to the list
+			newObj[key] = value;
+		}
+	});
+};
+
 export default class Interpreter {
 	constructor(ast, data) {
 		this.ast = ast;
@@ -12,28 +33,7 @@ export default class Interpreter {
 			: this.data
 					.map((d) => Core(this.ast, d))
 					.filter((i) => Object.keys(i).length >= 1)
-					.map(this.dive)
+					.map(dive)
 					.filter((i) => i);
-	}
-
-	// To remove all cases of empty obj's
-	dive(obj) {
-		const newObj = {};
-
-		Object.keys(obj).forEach((key) => {
-			const value = obj[key];
-
-			if (typeof value === 'object' && Object.keys(value).length >= 1) {
-				const deepDiveResult = dive(value);
-				if (Object.keys(deepDiveResult).length >= 1) {
-					// This means that there are valid children here...
-					newObj[key] = deepDiveResult;
-				}
-			} else if (value !== null && value !== undefined) {
-				// We are going to assume that this is some form of value... so we will
-				// add it to the list
-				newObj[key] = value;
-			}
-		});
 	}
 }
