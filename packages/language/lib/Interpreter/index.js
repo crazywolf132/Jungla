d8058555513adb46977b5d011a9db2f8995f9f7c
bmoon-autoit <bmoon@auto-it.com>
2021-05-13 16:33:39 +1000

    fix: Now wont return undefined or null objects

--- packages/language/lib/Interpreter/index.js
+++ packages/language/lib/Interpreter/index.js
@@ -17,7 +17,28 @@ class Interpreter {
   constructor(ast, data) {
     this.ast = ast;
     this.data = data;
-    this.result = !Array.isArray(this.data) ? (0, _core.default)(this.ast, this.data) : this.data.map(d => (0, _core.default)(this.ast, d)).filter(i => Object.keys(i).length >= 1);
+    this.result = !Array.isArray(this.data) ? (0, _core.default)(this.ast, this.data) : this.data.map(d => (0, _core.default)(this.ast, d)).filter(i => Object.keys(i).length >= 1).map(this.dive).filter(i => i);
+  } // To remove all cases of empty obj's
+
+
+  dive(obj) {
+    const newObj = {};
+    Object.keys(obj).forEach(key => {
+      const value = obj[key];
+
+      if (typeof value === 'object' && Object.keys(value).length >= 1) {
+        const deepDiveResult = dive(value);
+
+        if (Object.keys(deepDiveResult).length >= 1) {
+          // This means that there are valid children here...
+          newObj[key] = deepDiveResult;
+        }
+      } else if (value !== null && value !== undefined) {
+        // We are going to assume that this is some form of value... so we will
+        // add it to the list
+        newObj[key] = value;
+      }
+    });
   }
 
 }
--- packages/language/src/Interpreter/index.js
+++ packages/language/src/Interpreter/index.js
@@ -11,6 +11,29 @@ export default class Interpreter {
 			? Core(this.ast, this.data)
 			: this.data
 					.map((d) => Core(this.ast, d))
-					.filter((i) => Object.keys(i).length >= 1);
+					.filter((i) => Object.keys(i).length >= 1)
+					.map(this.dive)
+					.filter((i) => i);
+	}
+
+	// To remove all cases of empty obj's
+	dive(obj) {
+		const newObj = {};
+
+		Object.keys(obj).forEach((key) => {
+			const value = obj[key];
+
+			if (typeof value === 'object' && Object.keys(value).length >= 1) {
+				const deepDiveResult = dive(value);
+				if (Object.keys(deepDiveResult).length >= 1) {
+					// This means that there are valid children here...
+					newObj[key] = deepDiveResult;
+				}
+			} else if (value !== null && value !== undefined) {
+				// We are going to assume that this is some form of value... so we will
+				// add it to the list
+				newObj[key] = value;
+			}
+		});
 	}
 }
